0376628010aa165ed95eaa32bc160724e3dc3866
Isaac Ashwin <ashiswin@gmail.com>
2024-01-21 07:49:13 -0800

    Initial commit

---
+++ compress.py
@@ -0,0 +1,91 @@
+import zlib
+import binascii
+import subprocess
+import os
+from shutil import move, rmtree
+
+INPUT_FILENAME = "00000000000000000000000000000001.sav"
+SAMPLE_FILENAME = "51D4676E000000000000000000000000.sav"  # The file created when you first join your dedicated server
+
+INPUT_EXTRACTION_DIR = INPUT_FILENAME.split('.')[0]
+SAMPLE_EXTRACTION_DIR = SAMPLE_FILENAME.split(".")[0]
+# Extract existing saves
+try:
+    os.mkdir(INPUT_EXTRACTION_DIR)
+except FileExistsError:
+    pass
+
+try:
+    os.mkdir(SAMPLE_EXTRACTION_DIR)
+except FileExistsError:
+    pass
+
+subprocess.run(
+    ["offzip.exe", "-s", "-a", "-o", INPUT_FILENAME, INPUT_EXTRACTION_DIR]
+)
+subprocess.run(
+    ["offzip.exe", "-s", "-a", "-o", SAMPLE_FILENAME, SAMPLE_EXTRACTION_DIR]
+)
+
+PLAYER_UID_GUID_OFFSET = 0x6D2
+INDIVIDUAL_ID_GUID_OFFSET = 0x774
+INSTANCE_ID_GUID_OFFSET = 0x7C8
+
+steam_id = bytearray.fromhex(SAMPLE_EXTRACTION_DIR)[0:4]
+steam_id.reverse()
+
+print(steam_id)
+original_data = open(f"{INPUT_FILENAME.split('.')[0]}/0000000c.gva", "rb").read()
+sample_data = open(f"{SAMPLE_FILENAME.split('.')[0]}/0000000c.gva", "rb").read()
+
+original_bytearray = bytearray(original_data)
+sample_bytearray = bytearray(sample_data)
+
+# Overwrite player ID GUID
+original_bytearray[PLAYER_UID_GUID_OFFSET : PLAYER_UID_GUID_OFFSET + 4] = steam_id
+original_bytearray[INDIVIDUAL_ID_GUID_OFFSET : INDIVIDUAL_ID_GUID_OFFSET + 4] = steam_id
+
+print(
+    f"Updated Player UID GUID {hex(PLAYER_UID_GUID_OFFSET)}:{hex(PLAYER_UID_GUID_OFFSET + 4)} from {binascii.hexlify(original_data[PLAYER_UID_GUID_OFFSET:PLAYER_UID_GUID_OFFSET + 4])} to {binascii.hexlify(original_bytearray[PLAYER_UID_GUID_OFFSET:PLAYER_UID_GUID_OFFSET + 4])}"
+)
+print(
+    f"Updated Individual ID GUID from {hex(INDIVIDUAL_ID_GUID_OFFSET)}:{hex(INDIVIDUAL_ID_GUID_OFFSET + 4)} from {binascii.hexlify(original_data[INDIVIDUAL_ID_GUID_OFFSET:INDIVIDUAL_ID_GUID_OFFSET + 4])} to {binascii.hexlify(original_bytearray[INDIVIDUAL_ID_GUID_OFFSET:INDIVIDUAL_ID_GUID_OFFSET + 4])}"
+)
+
+# Remove "01" byte from the "0000...0001.sav" ID
+original_bytearray[PLAYER_UID_GUID_OFFSET + 12] = 0
+original_bytearray[INDIVIDUAL_ID_GUID_OFFSET + 12] = 0
+print(
+    f"Reset 1 byte at {hex(PLAYER_UID_GUID_OFFSET + 12)} from {original_data[PLAYER_UID_GUID_OFFSET + 12]} to {original_bytearray[PLAYER_UID_GUID_OFFSET + 12]}"
+)
+print(
+    f"Reset 1 byte at {hex(INDIVIDUAL_ID_GUID_OFFSET + 12)} from {original_data[INDIVIDUAL_ID_GUID_OFFSET + 12]} to {original_bytearray[INDIVIDUAL_ID_GUID_OFFSET + 12]}"
+)
+
+# Overwrite instance ID for inventory
+original_bytearray[
+    INSTANCE_ID_GUID_OFFSET : INSTANCE_ID_GUID_OFFSET + 16
+] = sample_bytearray[INSTANCE_ID_GUID_OFFSET : INSTANCE_ID_GUID_OFFSET + 16]
+print(
+    f"Updated Instance ID GUID {hex(INSTANCE_ID_GUID_OFFSET)}:{hex(INSTANCE_ID_GUID_OFFSET + 16)} from {binascii.hexlify(original_data[INSTANCE_ID_GUID_OFFSET:INSTANCE_ID_GUID_OFFSET + 16])} to {binascii.hexlify(original_bytearray[INSTANCE_ID_GUID_OFFSET:INSTANCE_ID_GUID_OFFSET + 16])}"
+)
+
+compressed_data = zlib.compress(original_bytearray)
+
+uncompressed_len = len(original_data)
+compressed_len = len(compressed_data)
+
+move(SAMPLE_FILENAME, f"{SAMPLE_FILENAME}.old")
+
+f = open(SAMPLE_FILENAME, "wb")
+f.write(uncompressed_len.to_bytes(4, byteorder="little"))
+f.write(compressed_len.to_bytes(4, byteorder="little"))
+f.write(b"PlZ")
+f.write(bytes([0x31]))
+f.write(compressed_data)
+
+print("Cleaning up...")
+rmtree(INPUT_EXTRACTION_DIR)
+rmtree(SAMPLE_EXTRACTION_DIR)
+
+print(f"Successfully processed {INPUT_FILENAME} and wrote to {SAMPLE_FILENAME}\n\t{SAMPLE_FILENAME}.old contains the original save file from your dedicated server")
